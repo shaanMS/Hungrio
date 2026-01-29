c6af2aed9f277a8db54c5767d0c7e593c96c27d4
shaanMS <96082415+shaanMS@users.noreply.github.com>
2026-01-29 22:27:46 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,14 +9,5 @@ from .views import MeAPI
 #api group
 urlpatterns = [
     path("api/auth/me/", MeAPI.as_view(), name="me"),
-<<<<<<< HEAD
+
 ]
-
-=======
-    
-]
-
-# page url group
-
-
->>>>>>> f6ace98ee09aea45157ae2f31b5c08266e9b4b46
