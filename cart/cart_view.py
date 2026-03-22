3d44d2ee2fac60b8201ef8af504473c492232433
shaanMS <96082415+shaanMS@users.noreply.github.com>
2026-03-22 22:47:18 +0530

    Add comment to login_required decorator

--- cart/cart_view.py
+++ cart/cart_view.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-@login_required
+@login_required # login required 
 def cart_ui(request):
     return render(request, "cart/cart.html")
