b81e349430e6f0d58653cb484f16520fe4f47d04
shaanMS <96082415+shaanMS@users.noreply.github.com>
2026-03-24 16:03:22 +0530

    Update checkout.js

--- staticfiles/js/checkout.js
+++ staticfiles/js/checkout.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://hungrio-production.up.railway.app";
+const API_BASE = "https://hungrio-production-4564.up.railway.app";
 var orderId = null;
 // DOM Elements
 const orderItemsEl = document.getElementById("order-items");
